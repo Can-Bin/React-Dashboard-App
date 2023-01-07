a6c840f59c03edf27440748a6fadc0feacaaf21d
Can Bin <canbin60@gmail.com>
2023-01-07 13:27:19 +0300

    App.js edited

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {ColorModeContext, useMode} from "./theme"
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from "./scenes/global/Topbar"
+import Dashboard from "./scenes/dashboard"
+import {Routes, Route} from "react-router-dom"
+import Sidebar from "./scenes/global/Sidebar"
+
 
 
 const App = () => {
@@ -11,7 +16,23 @@ const App = () => {
             <ThemeProvider theme={theme}>
                 <CssBaseline/>
                 <div className='app'> 
-                    <main className='content'></main>
+                    <Sidebar/>
+                    <main className='content'>
+                        <Topbar/>
+                        <Routes>
+                            {/* <Route path='/' element={<Dashboard/>}/>
+                            <Route path='/team' element={<Team/>}/>
+                            <Route path='/contacts' element={<Contacts/>}/>
+                            <Route path='/invoices' element={<Invoices/>}/>
+                            <Route path='/form' element={<Form/>}/>
+                            <Route path='/bar' element={<Bar/>}/>
+                            <Route path='/pie' element={<Pie/>}/>
+                            <Route path='/line' element={<Line/>}/>
+                            <Route path='/faq' element={<Faq/>}/>
+                            <Route path='/geography' element={<Geography/>}/>
+                            <Route path='/calendar' element={<Calendar/>}/> */}
+                        </Routes>
+                    </main>
                 </div>
             </ThemeProvider>
         </ColorModeContext.Provider>
