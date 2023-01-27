d8fa5bfd9c845486163f9005ea1000907801cc36
Can Bin <canbin60@gmail.com>
2023-01-27 20:40:53 +0300

    dashboard updated

--- src/scenes/dashboard/index.js
+++ src/scenes/dashboard/index.js
@@ -20,20 +20,77 @@ const Dashboard = () => {
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard"/>
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: colors.blueAccent[700],
+              color: colors.grey[100],
+              fontSize: "14px",
+              fontWeight:"bold",
+              padding: "10px 20px"
+            }}
+          >
+            <DownloadOutlinedIcon sx={{mr: "10px"}}/>
+            Download Reports
+          </Button>
+        </Box>
       </Box>
-      <Box>
-        <Button
-          sx={{
-            backgroundColor: colors.blueAccent[700],
-            color: colors.grey[100],
-            fontSize: "14px",
-            fontWeight:"bold",
-            padding: "10px 20px"
-          }}
-        >
-          <DownloadOutlinedIcon sx={{mr: "10px"}}/>
-          Download Reports
-        </Button>
+        {/* GRID AND CHARTS */}
+      <Box sx={{
+        display:"grid",
+        gridTemplateColumns:"repeat(12, 1fr)",
+        gridAutoRow:"140px",
+        gap:"20px"
+      }}>
+        {/* ROW */}
+        <Box gridColumn="span 3" backgroundColor={colors.primary[400]} display="flex" alignItems="center" justifyContent="center">
+          <StatBox
+            title="12,361"
+            subtitle="Emails Sent"
+            progress="0.75"
+            increase="+14%"
+            icon={
+              <EmailIcon sx={{color: colors.greenAccent[600], fontSize: "26px"}}/>
+            }
+          />
+        </Box>
+        <Box gridColumn="span 3" backgroundColor={colors.primary[400]} display="flex" alignItems="center" justifyContent="center">
+          <StatBox
+            title="431,225"
+            subtitle="Sales Obtained"
+            progress="0.5"
+            increase="+21%"
+            icon={
+              <PointOfSaleIcon sx={{color: colors.greenAccent[600], fontSize: "26px"}}/>
+            }
+          />
+        </Box>
+        <Box gridColumn="span 3" backgroundColor={colors.primary[400]} display="flex" alignItems="center" justifyContent="center">
+          <StatBox
+            title="32,441"
+            subtitle="New Clients"
+            progress="0.30"
+            increase="+14%"
+            icon={
+              <PersonAddIcon sx={{color: colors.greenAccent[600], fontSize: "26px"}}/>
+            }
+          />
+        </Box>
+        <Box gridColumn="span 3" backgroundColor={colors.primary[400]} display="flex" alignItems="center" justifyContent="center">
+          <StatBox
+            title="1,325,134"
+            subtitle="Traffic Inbound"
+            progress="0.80"
+            increase="+43%"
+            icon={
+              <TrafficIcon sx={{color: colors.greenAccent[600], fontSize: "26px"}}/>
+            }
+          />
+        </Box>
+
+        
+
+        
       </Box>
     </Box>
 
